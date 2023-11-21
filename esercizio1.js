c4aeb3c2f569b117baac62525402eb5b823ddb75
GabLux <gabrielelucchetti@live.it>
2023-11-21 19:40:29 +0100

    js

---
+++ esercizio1.js
@@ -0,0 +1,25 @@
+let listaProdotti = [
+    {
+        id: 1,
+        nome: "Iphone",
+        prezzo: 1000,
+    },
+    {
+        id: 2,
+        nome: "Mac",
+        prezzo: 3000,
+    },
+
+    {
+        id: 3,
+        nome: "Scrivania",
+        prezzo: 200,
+    },
+    {
+        id: 4,
+        nome: "Lampadari",
+        prezzo: 600,
+    },
+]
+
+console.log(listaProdotti.sort((nome1,nome2)=> nome1.nome- nome2.nome))
