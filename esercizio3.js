a5069b44f9340e7fe3747c60e5197d980fe392f6
GabLux <gabrielelucchetti@live.it>
2023-11-22 18:13:44 +0100

    commenti al url splittato

--- esercizio3.js
+++ esercizio3.js
@@ -1,5 +1,6 @@
 let url="https://sitodiecommerce.com/paginaDelProdotto?idProdotto=12312312312&userId=314123&time=8999"
-
+/* SPLITTIAMO PRIMA L'URL DAL "?" */
 let urlSplittato1= url.split("?") 
+/* POI SPLITTIAMO IL RISULTATO DALLA "&"*/
 let urlSplittato2= urlSplittato1[1].split("&")
 console.log(urlSplittato2)
