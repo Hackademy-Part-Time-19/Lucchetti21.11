ebbff48204f2c9fedcbe8bf119e8367cdb5335d4
GabLux <gabrielelucchetti@live.it>
2023-11-22 18:24:47 +0100

    esercizi con commenti

--- esercizio2.js
+++ esercizio2.js
@@ -1,9 +1,14 @@
+/* FUNZIONE FILTRA PREZZO */
+
 function filtraPrezzo(listaProdotti){
+    /* CREO DUE VARIABILI PREZZO E LISTA FILTRATA FACENDO UN ARRAY */
     let prezzo=250
     let listaFiltrata= []
+
+    /* CICLO FOR PER LA VERIFICA */
     for ( let index =0 ; index < listaProdotti.length; index++) {
         
-        
+        /* E VEDIAMO SE PER OGNI ELEMENTO IL PREZZO E' MAGGIORE DI 250 */
         if (listaProdotti[index].prezzo>prezzo) {
             listaFiltrata[index]= listaProdotti[index]
         }
@@ -42,4 +47,5 @@ let listaProdotti = [
 
 
 console.log(filtraPrezzo(listaProdotti))
+/* FILTRIAMO IL PREZZO SE E' MAGGIORE DI 250 CON ARROW FUNCTION */
 console.log(listaProdotti.filter((listaPrezzo)=> listaPrezzo.prezzo>250))
