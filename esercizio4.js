bcf89fca9330f220aa3bc92fbe9dcbd552eb36c8
GabLux <gabrielelucchetti@live.it>
2023-11-22 18:10:26 +0100

    Frase palindroma con commenti

--- esercizio4.js
+++ esercizio4.js
@@ -1,7 +1,13 @@
+
+/* DOBBIAMO PRENDERE LA FRASE */
 function palindroma(frase) {
+    /*TOGLIERE GLI SPAZI */
     let stringa= /\W/g;
+    /* METTERLA TUTTA IN PICCOLO */
     let stringaModificata = frase.toLowerCase().replace(stringa, '');
+    /*SPLITTARLA IN UN ARRAY INVERTIRE LA FRASE E RIUNIRLA    */
     let stringaInversa = stringaModificata.split('').reverse().join('');
+    /*FAR TORNARE COME RISULTATO TRUE/FALSE   */
     return stringaInversa===stringaModificata
 
     
